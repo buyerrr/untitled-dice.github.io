589ad205fe686b9fa3b0107d432e950623e7d475
tentknk <tentknk1212@gmail.com>
2016-11-13 11:25:09 +0700

    Update app.js

--- app.js
+++ app.js
@@ -6,10 +6,10 @@ var config = {
   // - Your app's id on moneypot.com
   app_id: 2405,                             // <----------------------------- EDIT ME!
   // - Displayed in the navbar
-  app_name: 'Untitled Dice',
+  app_name: 'Dice Chain',
   // - For your faucet to work, you must register your site at Recaptcha
   // - https://www.google.com/recaptcha/intro/index.html
-  recaptcha_sitekey: '6LfI_QUTAAAAACrjjuzmLw0Cjx9uABxb8uguLbph',  // <----- EDIT ME!
+  recaptcha_sitekey: '6Lf50AsUAAAAAHKkk_UukhfaEGSUU1uCBacexNH3',  // <----- EDIT ME!
   redirect_uri: 'https://untitled-dice.github.io',
   mp_browser_uri: 'https://www.moneypot.com',
   mp_api_uri: 'https://api.moneypot.com',
